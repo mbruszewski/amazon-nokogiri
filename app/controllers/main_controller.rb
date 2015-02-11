f04656c9bbcbdd350717ed0d8984863d70d01579
Mariusz <m.bruszewski91@gmail.com>
2015-02-11 12:22:22 +0100

    fixing review getter

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -17,22 +17,21 @@ class MainController < ApplicationController
 
     # get review div
     rev_helper    = reviews.at_css("#productReviews td div")
-    # get 10 comments from page
-    10.times do
-      # get review information
-      rev_title     = rev_helper.at_css("span b").text
-      rev_desc      = rev_helper.at_css(".reviewText").text
-      rev_mark      = rev_helper.at_css(".swSprite").text.split(" ")[0]
-      # create review
-      rev           = Review.create(title: rev_title, description: rev_desc, mark: rev_mark)
-      # add review to album
-      album.reviews << rev
-      # scroll to next review
-      7.times do
-        rev_helper = rev_helper.next_sibling if rev_helper
+    while rev_helper do
+      # check if we got enum at review
+      if rev_helper.at_css("span b")
+        # get review information
+        rev_title     = rev_helper.at_css("span b").text
+        rev_desc      = rev_helper.at_css(".reviewText").text
+        rev_mark      = rev_helper.at_css(".swSprite").text.split(" ")[0]
+        # create review
+        rev           = Review.create(title: rev_title, description: rev_desc, mark: rev_mark)
+        # add review to album
+        album.reviews << rev
       end
+      # scroll to next review
+      rev_helper = rev_helper.next_sibling
     end
-
     debugger
   end
 end
